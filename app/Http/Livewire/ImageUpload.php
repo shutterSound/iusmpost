c922c4adee06ff4fa81f7615c10f72dbc63c8ac2
DESKTOP-5GOR23V\Administrator <cybervit7@gmail.com>
2021-04-28 16:41:43 +0900

    사진보정2

--- app/Http/Livewire/ImageUpload.php
+++ app/Http/Livewire/ImageUpload.php
@@ -3,26 +3,35 @@ namespace App\Http\Livewire;
 
 use App\Models\Photo;
 use Illuminate\Support\Facades\Storage;
+use League\CommonMark\Inline\Element\Image;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 class ImageUpload extends Component
 {
     use WithFileUploads;
+//    코드수정 => input으로 넘겨받는 화일들 저장
     public $photos = [];
+//    코드수정 => 검색일자 처리
     public $searchDate ;
 
 
+//코드수정 =>  input으로 넘겨받은 화일들을 임시폴더에  저장한 목록들을 저장
     public $storeImages = [];
+//    코드수정 => 임시폴더명
     public $folder = '';
+//    코드수정 => 트리거 =>  다운로드 와 사진보정
     public $pasDown=[];
     public $pasProcess=[];
+    public $fileSelected;
 
     public function mount()
     {
         // 코드수정 => 초기값 설정
         $this->searchDate = date('Y-m-d');
+//        코드수정 => 트리거 초기화
         $this->pasDown = array_fill(0,99,0);
         $this->pasProcess = array_fill(0,99,1);
+        $this->fileSelected = false;
     }
 
     public function PasProcess($index)
@@ -51,7 +60,7 @@ class ImageUpload extends Component
 //        Storage::disk('public')->delete('public/'. $deletefile);
 
 //        삭제가 정상 작동한다.
-        @unlink('public/'. $deletefile);
+        @unlink('/public/'. $deletefile);
 
 //        코드수정 => 아래처럼 하면 선택한 배열요소를 삭제하고 나머지를 리턴한다.
 //       array_splice($files, $index,1 );
@@ -87,6 +96,9 @@ class ImageUpload extends Component
         Photo::create(['dataphoto' => $this->photos]);
         session()->flash('message', '이미지저장!');
         $this->photos='';
+
+//        코드수정 => 화일을 다시 입력 받을수 있도록 트리거
+        $this->fileSelected = false;
         // 화일들을 레코드에 저장하면 임시폴더와 화일들을 삭제한다.
         $this->allDeleteFile($this->folder);
 //        코드수정=> 리다이렉팅
@@ -119,6 +131,8 @@ class ImageUpload extends Component
 //        unset( $this->storeImages[$index] );
         array_splice($this->storeImages, $index,1 );
 //        dd($this->storeImages);
+//        코드수정 => 대기상태에서 삭제한 목록을 선택목록으로 대체하면 저장시 삭제한 항목이 처리된 후 저장됨 . 생략시 선택한게 모두 저장됨
+//        $this->photos = $this->storeImages;
 
 //        코드수정 => 다운로드 버튼 다 숨김
 //        $this->pasDown[ $index ] = 0;
@@ -134,12 +148,14 @@ class ImageUpload extends Component
     public function updatedphotos()
     {
 //        dd($this->photos );
+//        코드수정 => 화일을 선택했다는 트리거 활성화
+        $this->fileSelected = true;
         // 코드수정 => 화일을선택하면 특정폴더에 먼저 저장하고 그폴더의 이미지들을 목록으로 전달한다.
         if( $this->photos ){
             $this->folder = uniqid().Date('Y-m-d');
 
             $this->validate(
-                [   'photos.*' => 'image|max:1024'],
+                [   'photos.*' => 'image|max:10240'],
                 [ '미리보기 => 화일크기가 너무 큽니다.']
             );
             foreach($this->photos as $key => $photo ){
@@ -147,12 +163,22 @@ class ImageUpload extends Component
                 //    코드수정 => 원본화이름을 가지고 공백을제거하고 유니크아이디를 부여하여 작업수행
                 $fileName =  preg_replace("/\s+/","", $photo->getClientOriginalName());
                 $fileName = uniqid().'--'.$fileName;
-                $photo->storeAs($this->folder,$fileName, 'public');
+                $photo->storeAs( $this->folder,$fileName, 'public');
+
+//                $resizeFile = 'resize--'.$fileName;
+//                $icon = $photo->storeAs($this->folder,$resizeFile,'public');
+//                Image::make(Storage::disk('resize')->path($icon))->resize(200,200)->save();
+                $photo->storeAs( $this->folder,$fileName, 'public');
             }
+
 //    코드수정 => 다수의 화일이름들을 하나의 필드에 저장
 //            $this->photos = json_encode($this->photos);
             $this->storeImages = Storage::disk('public')->files($this->folder);
-//            dd($this->storeImages );
+//            $this->resizeImages = Storage::disk('resize')->files($this->folder);
+
+
+
+//            dd($this->storeImages , $this->photos , $this->resizeImages);
         }
 
         $this->pasDown= array_fill(0,count($this->storeImages),0);
--- resources/views/livewire/image-upload.blade.php
+++ resources/views/livewire/image-upload.blade.php
@@ -5,9 +5,13 @@
     @endif
     <form wire:submit.prevent ="saveimage" enctype="multipart/form-data">
         <input wire:model="searchDate" type="date" >
+
                                       {{-- 코드수정 => 원하는 화일 타입만 리스팅한다.--}}
+        @if(!$fileSelected)
         <input wire:model="photos" type="file"  accept="image/x-png,image/gif,image/jpeg" multiple>
         @error('photos.*')<div class="text-danger">{{$message}}</div> @enderror
+        @endif
+
         <button type="submit" >저장</button>
     </form>
 
@@ -52,13 +56,16 @@
               @if(strlen($file) > 2)
                     @if(count(explode(",",$photo->dataphoto)) == 1 )
                         <a href="{{'/public/'.substr($file,2,-2)}}">
-                        <img src="{{'/public/'.substr($file,2,-2)}}" width='100px'/></a>
+                        <img src="{{'/public/'.substr($file,2,-2)}}" width='100px'/>
+                            {{substr(substr($file,2,-2), strpos(substr($file,2,-2), "--")+2) }}</a>
                     @elseif( ($loop->index + 1) == count(explode(",",$photo->dataphoto)) &&  ($loop->index + 1) > 1  )
                         <a href="{{'/public/'.substr($file,2,-2)}}">
-                        <img src="{{'/public/'.substr($file,2,-2)}}" width='100px'/></a>
+                        <img src="{{'/public/'.substr($file,2,-2)}}" width='100px'/>
+                            {{substr(substr($file,2,-2), strpos(substr($file,2,-2), "--")+2) }}</a>
                     @else
                         <a href="{{'/public/'.substr($file,2,-1)}}">
-                        <img src="{{'/public/'.substr($file,2,-1)}}" width='100px'/></a>
+                        <img src="{{'/public/'.substr($file,2,-1)}}" width='100px'/>
+                            {{substr(substr($file,2,-1), strpos(substr($file,2,-1), "--")+2) }}</a>
                     @endif
 {{--                   코드수정 => 레코드 번호와 선택사진 인덱스를 전달하여 요소를 삭제한다.  --}}
                     <buutton wire:click.prevent="removeRecodeImage({{$photo->id}},{{$loop->index}})"
